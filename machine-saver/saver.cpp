c8233a29b0c1d91c55ee314d8bf32a7fda3864f3
Eran Ben Horin <eran@observeit-sys.com>
2013-05-29 19:56:02 +0300

    machine-saver
    its a start

---
+++ machine-saver/saver.cpp
@@ -0,0 +1,13 @@
+/*
+* this server has a very simple role - save machines that got bitten by
+* the obit bad module.
+*/
+
+/*
+* class explorer
+* used to explor the locked machine
+*/
+class explorer 
+{
+  
+};
