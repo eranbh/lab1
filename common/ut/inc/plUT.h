050637032bcb4e3c68aaffa0d4c8d90cecf5961f
Eran Ben-Horin <eran.benhorin@gmail.com>
2013-10-13 18:56:56 +0300

    common
    adding the pl tester

---
+++ common/ut/inc/plUT.h
@@ -0,0 +1,41 @@
+/*
+ * plUT.h
+ *
+ *  Created on: Oct 11, 2013
+ *      Author: eran
+ */
+
+#ifndef PLUT_H_
+#define PLUT_H_
+
+#include "cppunit/TestFixture.h"
+
+namespace pl{
+	namespace ut{
+
+class plUT : public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE(plUT);
+
+	CPPUNIT_TEST(test_stam);
+
+	CPPUNIT_TEST_SUITE_END();
+
+
+public:
+
+	plUT(){}
+	virtual ~plUT(){}
+	virtual void setUp();
+	virtual void tearDown();
+
+	void test_stam();
+};
+
+	}// namespace ut
+
+} // namespace pl
+
+
+
+#endif /* PLUT_H_ */
---
+++ common/ut/src/plUT.cpp
@@ -0,0 +1,24 @@
+/*
+ * plUT.cpp
+ *
+ *  Created on: Oct 11, 2013
+ *      Author: eran
+ */
+#include "cppunit/extensions/HelperMacros.h"
+#include "plUT.h" // my test cases
+namespace pl{
+	namespace ut{
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION(plUT);
+
+void plUT::setUp(){}
+void plUT::tearDown(){}
+
+void plUT::test_stam()
+{
+}
+
+	} // namespace ut
+
+} // namespace pl
