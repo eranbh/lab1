c376184f7b73775737f109f30cb41ec54fc944a1
Eran Ben-Horin <eran.benhorin@gmail.com>
2013-10-18 11:18:28 +0300

    common
    layed out the foundation for a pl module which will be used as a stub
    simulating the oit pl module.
    i plan on using it to assert the pl logic i nneed in the machine saver.
    as the pl symbols all come from libc [not under my control], ii had to
    find a way to _parameteriize_ them with a var desc'ing whether pl was
    indeed needed in each case.
    I thought of the following plan:
    for each call where pl is needed, a file will be written to disc.
    all syms in the stub module will assert the existance of these files.
    each file will bare the symbol's name [.e.g. open_file].

--- common/ut/src/plUT.cpp
+++ common/ut/src/plUT.cpp
@@ -3,9 +3,25 @@
  *
  *  Created on: Oct 11, 2013
  *      Author: eran
+ *
+ *  This test suit has a very important prerequisite:
+ *  	It has to have LD_PRELOAD [or its Unix equivalents]
+ *  	pointing to liboit_stubs.so
+ *  	If this is not the case, many [if not all] test cases
+ *  	will fail miserably.
+ *  	1. Launch from eclipse:
+ *  		add the path to the env tab of the
+ *  		execution/project/IDE.
+ *  		Note: you have an ooo execution called plUT for that.
+ *      2. Launch from shell
+ *         	export the var into the session/add it to you .profile
+ *         	Note: you have a script for that: plUT.sh
+ *     	This also means that any make that cooks this tester, *has* to
+ *     	build the liboit_stubs.so module, for the _forbidden_ symbols
  */
 #include "cppunit/extensions/HelperMacros.h"
 #include "plUT.h" // my test cases
+
 namespace pl{
 	namespace ut{
 
@@ -17,7 +33,8 @@ void plUT::tearDown(){}
 
 /*
 * linux employs a system-wide strategy for pre-loading.
-*
+* this means that no amount of trickery will help before
+* an exec, as the loader simply appends the pre-loading anyway
 */
 void plUT::test_linux()
 {
