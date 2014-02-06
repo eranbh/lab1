5fa156ce3e27657c13e4005c35b7157ff8df7926
Eran Ben Horin <eran@observeit-sys.com>
2014-02-06 19:48:47 +0200

    common
    added a trxmitter class. i need this for the reactor.
    one thing is that this needs to be generic - could work with tcp
    unx sockets at least - as i need both ...
    anyway, for now it will support unx sockets - thats what i need between
    the reactor and obitd

---
+++ common/nw/inc/Transmitter.h
@@ -0,0 +1,30 @@
+/*
+ * Transmitter.h
+ *
+ *  Created on: Feb 6, 2014
+ *      Author: eranbh
+ */
+
+#ifndef __NW_INC_TRANSMITTER_H_
+#define __NW_INC_TRANSMITTER_H_
+
+namespace nw{
+
+  namespace ut{
+    class nwUT;
+  }
+
+/*
+* this is a simple transmitter class.
+*/
+class Transmitter
+{
+};
+
+
+
+} // namespace nw
+
+
+
+#endif /* TRANSMITTER_H_ */
