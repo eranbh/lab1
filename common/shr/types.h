5b5e7a7f4e36618918f923f3163c8de0adebb5e8
Eran Ben Horin <eranbh@VENUS.(none)>
2014-06-03 19:37:04 +0300

    common
    	* added a generic container type to support the work im
    	  doing in the graph project

--- common/shr/types.h
+++ common/shr/types.h
@@ -44,6 +44,52 @@ typedef struct tBufferSz
         	char buff[1024];
   }BufferSz;
 
+
+/*FAIR WARNING - copies raw bytes.                                                                                                
+* if you want some fancy copy c'tor activated
+* write your own
+*/
+template <typename T>
+static void
+raw_copy_bytes(T* op_dest, const T* const ip_src)
+{
+  assert(op_dest &&  ip_src);
+  assert(::memcpy(op_dest, ip_src, sizeof(T)));
+}
+
+
+/*
+* By no means a rival to the stl. I just need it
+* when Im doing a quick impl for some other, more
+* lofty idea. *do not* rely on this for *anything*
+* that might involve MT'ing; needs effiecient handling
+* or any other harsh constraints of the sort 
+*/
+template<typename T>
+class GenContainer
+{
+ public:
+  /*need to access my privates when copying*/
+  friend class GenContainer;
+
+ GenContainer(const GenContainer& a_other):
+  m_sz(a_count.m_sz)
+ {
+   assert(a_other.m_sz && a_other.m_datum);
+
+   for(size_t i=0;i<a_sz;++i)
+   {
+     // it is assumed that the obj is copy'able
+     m_datum[i] = new T(a_other.m_datum[i]);
+   }
+ }
+  
+private:
+  unsigned int m_sz;
+  T* m_datum;
+};
+
+
 } // namespace fw
 
 #endif // __COMMON_SHR_TYPES_H
