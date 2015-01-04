28bc6ab3e7e32f0c864f5f48dfbdbb49c08f3c84
Eran Ben Horin <eran@observeit-sys.com>
2015-01-04 06:05:33 -0800

    python
    continue with the ibm task.
    need to get a good indication for module loading failure

--- python/ibm/CoreCommands.py
+++ python/ibm/CoreCommands.py
@@ -1,24 +1,24 @@
 """ this module contains the out-of-the-box modules
     supported by the system. these modules are supplied
     as part of the package and are not / can not be
-    subjected to any customizations. 
+    subjected to any customizations.
 
-    any client of this mechanism who wishes create 
+    any client of this mechanism who wishes create
     a customized command, needs to adhere to the FileManipulationCmd
     interface defined here """
 
 from abc import ABCMeta, abstractmethod
 
 class FileManipulationCmd:
-    
-    __metaclass__ = ABCMeta
-    mFileManager
-    def __init__(fileManager):
-      mFileManager = fileManager  
-    
-    @abstractmethod
-    def execute(self)
 
+    __metaclass__ = ABCMeta
+    mFileManager = ''
+    def __init__(fileManager):
+      mFileManager = fileManager
+
+    @abstractmethod
+    def execute(self):
+        pass
 
 
 class Sort(FileManipulationCmd):
@@ -30,10 +30,10 @@ class Sort(FileManipulationCmd):
 
 
 class Reverse(FileManipulationCmd):
-    
+
     def __init__(self, fileManager):
         Base.__init__(self, fileManager)
-    
+
     def execute(self):
         Base.__init__(self, fileManager)
         print "Reverse [Execute]"
--- python/ibm/FileManipulator.py
+++ python/ibm/FileManipulator.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 
+import os
 import sys, getopt
 import CoreCommands
+import imp
 
-def usage():
+def usage(reason):
+    print 'program will halt:' +  reason
     print 'usage: FileManipulator.py -x <command> -i <input-file> -o <output-file>'
 
 
@@ -16,15 +19,15 @@ class FileManagment:
     def prepareFiles(self, inFile, outFile):
 
         miFile = open(inFile, 'r')
-        
+
         # if we did not get an out-file we use stdout
         moFile = sys.stdout
         if outFile != '':
             moFile = open(outFile, 'w')
 
 def main(argv):
-    if len(sys.argv) < 3 or len(sys.argv) > 4:
-        usage()
+    if (len(sys.argv) < 5) or (len(sys.argv) > 7):
+        usage('bad amount of command line arguments')
         exit(1)
     inputFile=''
     outputFile=''
@@ -33,20 +36,43 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv, "x:i:o") #TODO support long options too
     except getopt.GetoptError:
-        usage() #TODO also get the reason for the exception
+        usage('bad opt switch encountered') #TODO get exception reason
+        exit(1)
 
     for opt, arg in opts:
-        if opt == -x:
+        if opt == '-x':
             command = arg
-        elif opt == -i:
+        elif opt == '-i':
             inputFile = arg
-        elif opt == -o:
+        elif opt == '-o':
             outputfile = arg
-    fileManager = FileManagment()
-    fileManager.prepareFiles(inputFile, outputFile)
+
+    # handle a situation where a switch repeated itself.
+    # i think this is a bug in the getopt logic. it should
+    # trace the fact that a certain switch was already supplied
+    if command == '' or inputFile == '':
+        usage('mandatory switch was not given')
+        exit(1)
+
+    try:
+        fileManager = FileManagment()
+        fileManager.prepareFiles(inputFile, outputFile)
+    except Exception as e:
+        print "preparing files for manipulation failed: %s" % e
+        exit(2)
 
     #run command executer
     
+    modSearchPath = os.getenv('MODULE_SEARCH_PATH', os.getcwd()+ '/cmd')
+    commandModule = imp.load_source(command, modSearchPath)
+    if commandModule == None:
+        print "module %s was not found" % command
+        exit(2)
+    from commandModule import command
+    cmd = eval(command+"()")
+    cmd.__init__(fileManager)
+    cmd.execute()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
